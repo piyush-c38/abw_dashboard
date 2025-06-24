28681ca33da59f7f7115703015aae6bdd6142791
piyush-c38 <chandrakarpiyush36@gmail.com>
2025-06-24 16:43:22 +0530

    Created the basic UI of home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
+"use client";
 import Image from "next/image";
 import { Button } from "@/components/ui/button"
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const router = useRouter();
+
   return (
     <>
       <div className="w-screen h-screen flex justify-center items-center relative "
@@ -10,17 +14,25 @@ export default function Home() {
           backgroundSize: "cover",
           backgroundPosition: "center"
         }}>
-        {/* Black blur overlay */}
-        <div className="absolute inset-0 bg-black/20 z-0"></div>
-        <div className="w-3/4 h-3/4 bg-gray-50 z-10 rounded-2xl">
-          <Image
-            src="/logo_506.png"
-            alt="Logo_506"
-            width={120}
-            height={120}
-            className="mb-6"
-          />
-          <Button>Button</Button>
+        <div className="absolute inset-0 bg-black/20 z-0" />
+        <div className="w-3/4 h-3/4 bg-gray-50 z-10 rounded-2xl flex flex-col justify-around">
+          <div className="flex justify-center w-full">
+            <div className="w-auto flex justify-center items-center gap-10 p-3">
+              <Image
+                src="/logo_506.png"
+                alt="Logo_506"
+                width={120}
+                height={120}
+              />
+              <div className = "text-2xl text-shadow-black"> 506 Army Base Workshop</div>
+            </div>
+          </div>
+          <div className="flex justify-center">
+            <div className="flex flex-wrap gap-10 justify-center">
+              <Button className="w-50 h-15 text-xl" onClick={() => router.push("/todayjob")}>Today's Job</Button>
+              <Button className="w-50 h-15 text-xl" onClick={() => router.push("/daysummary")}>Day Summary</Button>
+            </div>
+          </div>
         </div>
       </div>
     </>
